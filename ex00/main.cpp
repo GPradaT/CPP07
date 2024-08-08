376bbb4db7d00d2754553fe78c9b37a0092a0fa3
Guillem Prada Torres <gprada-t@car10s2.42barcelona.com>
2024-08-08 15:48:57 +0200

    ex00 done

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,43 +1,58 @@
-#include <iostream>
-
-template<typename T>
-T	max(T const &x, T const &y)
-{
-	return ((x >= y) ? x : y);
-}
-
-template<typename T>
-T	min(T const &x, T const &y)
-{
-	return ((x <= y) ? x : y);
-}
-
-template<typename T>
-void	swap(T *x, T *y)
-{
-	T *temp;
-
-	temp = x;
-	x = y;
-	y = temp;
-}
+#include "whatever.hpp"
 
 int	main( void )
 {
-	int	a = 3;
-	int	b = 65;
-	float	a1 = 72;
-	float	b1 = 23;
-	double	a2 = 6;
-	double	b2 = 45;
-	unsigned int a3 = 13;
-	unsigned int b3 = 61;
-
-	std::cout << max(a, b) << std::endl;
-	std::cout << min(a1, b1) << std::endl;
-	::swap(a2, b2);
-	std::cout << a2 << std::endl;
-	std::cout << b2 << std::endl;
-	std::cout << "=============" << std::endl;
-	return 0;
+	{
+		int a = 2;
+		int b = 3;
+		::swap( a, b );
+		std::cout << "a = " << a << ", b = " << b << std::endl;
+		std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+		std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+		std::string c = "chaine1";
+		std::string d = "chaine2";
+		::swap(c, d);
+		std::cout << "c = " << c << ", d = " << d << std::endl;
+		std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+		std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	}
+	{
+		int	a = 3;
+		int	b = 65;
+		float	a1 = 72;
+		float	b1 = 23;
+		double	*a2 = new double();
+		*a2 = 6;
+		double	*b2 = new double();
+		*b2 = 45;
+		unsigned int *a3 = new unsigned int();
+		*a3 = 13;
+		unsigned int *b3 = new unsigned int();
+		*b3 = 61;
+	
+		std::cout << "=======================" << std::endl;
+		std::cout << "a = " << a << std::endl << "b = "<< b << std::endl;
+		std::cout << "max(a, b) 	= " << ::max(a, b) << std::endl;
+		std::cout << "a1 = " << a1 << std::endl << "b1 = "<< b1 << std::endl;
+		std::cout << "min(a1, b1) 	= " << ::min(a1, b1) << std::endl;
+		std::cout << "=======SWAP DEMO=======" << std::endl;
+		std::cout << "Value of a2	= " << *a2 << std::endl;
+		std::cout << "Value of b2	= " << *b2 << std::endl;
+		std::cout << "Value of a3	= " << *a3 << std::endl;
+		std::cout << "Value of b3	= " << *b3 << std::endl;
+		std::cout << "=======================" << std::endl;
+		::swap(*a2, *b2);
+		::swap(*a3, *b3);
+		std::cout << "Value of a2	= " << *a2 << std::endl;
+		std::cout << "Value of b2	= " << *b2 << std::endl;
+		std::cout << "Value of a3	= " << *a3 << std::endl;
+		std::cout << "Value of b3	= " << *b3 << std::endl;
+		std::cout << "=======================" << std::endl;
+	
+		delete a2;
+		delete a3;
+		delete b2;
+		delete b3;
+	}	
+		return 0;
 }
